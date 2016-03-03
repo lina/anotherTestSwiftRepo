10c73f798480f496ebf502763c07ba46cec2eb71
Lina Lu <linalu001@gmail.com>
2016-03-03 01:29:47 -0800

    Initial Commit

---
+++ anotherTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anotherTestProject
+//
+//  Created by Yaqing LU on 3/3/16.
+//  Copyright © 2016 LinaLu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
